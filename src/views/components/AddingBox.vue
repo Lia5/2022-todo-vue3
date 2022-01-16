0c162852c89ce42a27bc2bcf52df971f1ff5f580
lia5 <liaforweb@gmail.com>
2022-01-16 18:57:36 +0200

    validation

--- src/views/components/AddingBox.vue
+++ src/views/components/AddingBox.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <div class="main-title">{{ this.$t("main_title") }}</div>
-        <div class="adding-box">
+        <div class="adding-box" :class="isShow ? 'show' : '' ">
             <input id="todo-field" type="text" placeholer="Введіть текст" v-bind:value="valueInput" v-on:input="handlyInput" v-on:keypress.enter="addTask">
+            <div class="error" v-if="error">{{ this.$t("error_text") }}</div>
             <div class="btns">
                 <button class="btn btn--primary" v-on:click="addTask">{{ this.$t("general.save") }}</button>
                 <button class="btn btn--danger" v-on:click="hideAddingBox">{{ this.$t("general.cancel") }}</button>
@@ -21,15 +22,16 @@ export default {
     data() {
         return {
             valueInput: '',
-
+            isShow: false,
+            error: false
         }
     },
     methods: {
         showAddingBox () {
-            document.querySelector('.adding-box').classList.add('show');
+            this.isShow = true
         },
         hideAddingBox () {
-            document.querySelector('.adding-box').classList.remove('show');
+            this.isShow = false
             document.getElementById('todo-field').value="";
         },
 
@@ -37,13 +39,24 @@ export default {
             this.valueInput = event.target.value;
         },
         addTask () {
-            if(this.valueInput === '') { return }
+            if(this.valueInput === '') { 
+                this.error = true;
+                return 
+            }
             this.emitter.emit('addTaskItem', this.valueInput);
             this.valueInput = '';
+            this.error = false;
         },
     }
 }
 </script>
 <style lang="scss">
-
+    .error {
+        max-width: 268px;
+        margin: 0 auto;
+        text-align: left;
+        color: red;
+        padding-top: 5px;
+        font-size: 12px;
+    }
 </style>
